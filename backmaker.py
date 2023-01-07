5e1752ffd05f6541fbcd42750735fad29b249b38
t0tal <t0tal@security>
2023-01-07 04:28:40 +0100

    [ PATCH ] Refactored the code, made it more readable. Made logical changes to code functioning. ( v0.5.1 )

--- backmaker.py
+++ backmaker.py
@@ -5,16 +5,20 @@ import libs.systemHelpers as sH
 
 def printLogo() -> None:
 	try:
-		file_Logo = AsciiObject("visuals/logo.txt")
+		file_Logo = AsciiObject("visualss/logo.txt")
 	except ValueError as VE:
 		print(VE)
+	except FileNotFoundError as FNFE:
+		print(f"Can not display logo: {FNFE}")
 	else:
 		file_Logo.printFileContent()
 
 
-def printDefaultChoice() -> None:
+def printDefaultChoice() -> list:
 	print("\nMake a choice of what to backup:")
 	print("\n[1] File\n[2] Directory\n[3] List\n\n[0] Exit\n")
+
+	return [1, 2, 3]
 
 
 def getMenuChoice(F_go_back: bool=False) -> int:
@@ -36,42 +40,34 @@ def getMenuChoice(F_go_back: bool=False) -> int:
 def backupFile() -> None:
 	paths = []
 	try:
-		file_Path = AsciiObject(input("\nProvide absolute file path: "))
-		paths.append(file_Path)
+		try:
+			file_Path = AsciiObject(input("\nProvide absolute file path: "))
+			paths.append(file_Path)
+		except ValueError:
+			print(f"Specify the absolute path to the file you want to backup!")
+			exit()
+		
+		try:
+			backup_Path = AsciiObject(input("Provide absolute backup path: "))
+			paths.append(backup_Path)
+		except ValueError:
+			print(f"Specify the absolute path to the path where you want to backup your file!")
+			exit()
 
-		backup_Path = AsciiObject(input("Provide absolute backup path: "))
-		paths.append(backup_Path)	
-
-		for path in paths:
-			if path == False:
-				print(f"\nPath must be absolute!\nThis one '{path}' is not")
-				exit()	
-	except ValueError:
-		print(f"Specify the absolute path to the file you want to backup!")
-		exit()
 	except KeyboardInterrupt as KI:
 		print("\nTerminating!\n")
 		exit()
+			
 
 
 def main() -> None:
-	#sH.clearScreen()
-	#printLogo()
-	#printDefaultChoice()
+	sH.clearScreen()
+	printLogo()
+	printDefaultChoice()
 
 	choice = getMenuChoice()
 
-	if choice == 1:		
-		backupFile()
-	elif choice == 2:
-		pass
-	elif choice == 3:
-		pass
-	elif choice == 0:
-		print("\nTerminating!\n")
-		exit()
-
-
+	
 
 if __name__ == "__main__":
 	main()
--- libs/handleAscii.py
+++ libs/handleAscii.py
@@ -17,11 +17,13 @@ class AsciiObject:
 
 
         self.file_Path = self.sanitizeUserInput(M_file_path)
+
+        if self.doesExistPath() == False:
+            raise FileNotFoundError(f"'{self.file_Path}' does not exist")
         
-    
+            
     def __str__(self) -> str:
         return self.file_Path
-
 
 
     def sanitizeUserInput(self, M_user_input: str) -> str:
@@ -45,54 +47,26 @@ class AsciiObject:
 
 
     def getHashSum(self, M_hash_type: str="") -> str:
-        # MD5, SHA1, SHA256, SHA512
-        hash_Type = self.sanitizeUserInput(M_hash_type)
+        hash_Type = self.sanitizeUserInput(M_hash_type).upper()
         
         if hash_Type == "":
             raise ValueError("Provide hash type")
 
-        def getHashMD5():
-            script_Absolute_Path = AsciiObject("libs/shell/getHashMD5.sh").getAbsolutePath()
+        hash_Types = ["MD5", "SHA1", "SHA256", "SHA512"]
 
-            hash_Sum = subprocess.check_output(["/usr/bin/bash", script_Absolute_Path, self.file_Path]).decode("utf-8")
-            return hash_Sum
-
-        def getHashSHA1():
-            script_Absolute_Path = AsciiObject("libs/shell/getHashSHA1.sh").getAbsolutePath()
-
-            hash_Sum = subprocess.check_output(["/usr/bin/bash", script_Absolute_Path, self.file_Path]).decode("utf-8")
-            return hash_Sum
-
-        def getHashSHA256():
-            script_Absolute_Path = AsciiObject("libs/shell/getHashSHA256.sh").getAbsolutePath()
-
-            hash_Sum = subprocess.check_output(["/usr/bin/bash", script_Absolute_Path, self.file_Path]).decode("utf-8")
-            return hash_Sum
-
-        def getHashSHA512():
-            script_Absolute_Path = AsciiObject("libs/shell/getHashSHA512.sh").getAbsolutePath()
-
-            hash_Sum = subprocess.check_output(["/usr/bin/bash", script_Absolute_Path, self.file_Path]).decode("utf-8")
-            return hash_Sum
-
-        hash_Types = {
-            "MD5":    getHashMD5,
-            "SHA1":   getHashSHA1,
-            "SHA256": getHashSHA256,
-            "SHA512": getHashSHA512
-        }
-
-        if hash_Type in hash_Types.keys():
-            hash_Value = hash_Types.get(hash_Type)()
+        if hash_Type in hash_Types:
+            script_Absolute_Path = AsciiObject(f"libs/shell/getHash{hash_Type}.sh").getAbsolutePath()
         
-        return hash_Value
+        try:
+            hash_Sum = subprocess.check_output(["/usr/bin/bash", script_Absolute_Path, self.file_Path]).decode("utf-8")
+        except UnboundLocalError:
+            print("Check provided hash type again!")
+        else:
+            return hash_Sum
 
 
     def readFileContent(self) -> str:
         absolute_File_Path = self.getAbsolutePath()
-
-        if self.doesExistPath() == False:
-            raise FileNotFoundError(f"Provided non-existent file '{absolute_File_Path}' to read")
             
         with open(absolute_File_Path, "r") as file_To_Read:
                 file_Content = file_To_Read.read()
@@ -100,23 +74,14 @@ class AsciiObject:
         return file_Content
 
 
-    def printFileContent(self):
-        try:
-            file_Content = self.readFileContent()
-        except FileNotFoundError as FNFE:
-            print(FNFE)
-            exit()
-        else:
-            print(file_Content) 
+    def printFileContent(self) -> None:
+        file_Content = self.readFileContent()
+        print(file_Content) 
 
 
 
 if __name__ == "__main__":
-    AO = AsciiObject("visuals/logso.txt")
-    #print(AO.getAbsolutePath())
-    #AO.printFileContent()
-    #try:
-    print(AO.getHashSum("SHA512"))
-    #except ValueError as VE:
-        #print(VE)
+    AO = AsciiObject("visuals/logo.txt")
+    
+    print(AsciiObject("visuals/logo.txt").getHashSum("sha512"))
     
